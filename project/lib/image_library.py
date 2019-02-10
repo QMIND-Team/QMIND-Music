f328ceb1ba945bc6f0b8aab99f880b47bd92bfd6
Joey Tepperman <joeytepperman@Joeys-MacBook-Air.local>
2019-02-10 13:04:17 -0500

    Add libary files

---
+++ project/lib/image_library.py
@@ -0,0 +1,8 @@
+"""
+A library for all functions image-related
+"""
+
+
+# Turn an image file into an array of features
+def get_image_features(image_path):
+    pass
---
+++ project/lib/music_library.py
@@ -0,0 +1,13 @@
+"""
+A library for all music-related functions
+"""
+
+
+# Takes an mp3 song as input and outputs a midi file
+def convert_mp3_to_midi(song_path, midi_path):
+    pass
+
+
+# Takes a midi song as an input and outputs an array of features
+def get_song_features(midi_path):
+    pass
